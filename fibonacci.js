cf0378fffc81e3591768802d8c8ce0201deba839
Yelish <118233081+GrapeJuice11111@users.noreply.github.com>
2024-01-04 01:16:10 -0800

    Commit A More Elegant Solution For Fibonacci Recursion

--- fibonacci.js
+++ fibonacci.js
@@ -1,15 +1,11 @@
 function fibonacci(n) {
-  let arr = [0, 1];
-  function fibonacciRecursion(n) {
-    if (n == 1) {
-      return arr;
-    } else {
-      let copy = arr.slice();
-      arr.push(copy.pop() + copy.pop());
-      return fibonacciRecursion(n - 1);
-    }
+  if (n == 1) {
+    return [0, 1];
+  } else {
+    let array = fibonacci(n - 1);
+    let copy = array.slice();
+    array.push(copy.pop() + copy.pop());
+    return array;
   }
-  return fibonacciRecursion(n);
 }
-console.log(fibonacci(28));
 console.log(fibonacci(100));
